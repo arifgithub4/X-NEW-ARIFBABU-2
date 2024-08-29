63fcb40053b48146d0820794fdf823137e5477a5
YOUTUBE-TACH <newgithub@yandex.com>
2024-08-29 20:07:46 +0530

    Update 0_ARIF-ASSALAM-MP3.js

--- modules/commands/0_ARIF-ASSALAM-MP3.js
+++ modules/commands/0_ARIF-ASSALAM-MP3.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "Assalamualaikum",
     version: "1.1.1",
   hasPermssion: 0,
-  credits: "PREM BABU", 
+  credits: "ARIF BABU", 
   description: "Just Respond",
   commandCategory: "no prefix",
     cooldowns: 5, 
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("ASSALAM") ||
 react.includes("aSSALAMUALAIKUM")) {
     var msg = {
-        body: `‌𝐖𝐀𝐋𝐀𝐈𝐊𝐔𝐌 𝐀𝐒𝐒𝐀𝐋𝐀𝐌 𝐖𝐀 𝐑𝐀𝐇𝐌𝐀𝐓𝐔𝐋𝐋𝐀𝐇𝐈 𝐁𝐀𝐑𝐀𝐊𝐀𝐓𝐔𝐇 🤗💫`,attachment: fs.createReadStream(__dirname + `/noprefix/assalamualaikum.mp3`)
+        body: `‌𝐖𝐀𝐋𝐀𝐈𝐊𝐔𝐌 𝐀𝐒𝐒𝐀𝐋𝐀𝐌 𝐖𝐀 𝐑𝐀𝐇𝐌𝐀𝐓𝐔𝐋𝐋𝐀𝐇𝐈 𝐁𝐀𝐑𝐀𝐊𝐀𝐓𝐔𝐇 🤗💫`,attachment: fs.createReadStream(__dirname + `/ARIF-BABU/assalamualaikum.mp3`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("💫", event.messageID, (err) => {}, true)
