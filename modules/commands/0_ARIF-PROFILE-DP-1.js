38a3df143b176c31bcacf8fc844dd10175760cb9
YOUTUBE-TACH <newgithub@yandex.com>
2024-08-30 10:41:05 +0530

    Update 0_ARIF-PROFILE-DP-1.js

--- modules/commands/0_ARIF-PROFILE-DP-1.js
+++ modules/commands/0_ARIF-PROFILE-DP-1.js
@@ -2,8 +2,8 @@ module.exports.config = {
   name: "dp",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "PREM BABU",
-  description: "THIS BOT IS MADE BY PREM BABU",
+  credits: "ARIF BABU",
+  description: "THIS BOT IS MADE BY ARIF BABU",
   commandCategory: "PROFILE DP",
   usages: "SELF DP AND MENTION",
   dependencies: {
